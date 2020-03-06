1fc609f165aa908ae9d0acb0145cf6921f64d2ec
SuelyBarreto <suely@barreto.us>
2020-03-05 19:02:26 -0800

    palindrome and longest prefix

--- lib/practice_exercises.rb
+++ lib/practice_exercises.rb
@@ -1,13 +1,35 @@
+#!/usr/bin/ruby
+# 
 
-# Time Complexity: ?
-# Space Complexity: ?
+# Given a string, determine if it is a palindrome considering only alphanumeric characters and ignoring cases
+# Note: For the purpose of this problem, we define empty string as valid palindrome.
+
+# Time complexity: O(n)
+# Space complexity: O(1)
 def is_palindrome(string)
-  raise NotImplementedError, "Not implemented yet"
+  string = string.downcase.gsub(/[^a-z]/i, '')
+
+  i = 0
+  j = string.length - 1
+  while i < j do
+    return false if string[i] != string[j]
+    i += 1
+    j -= 1
+  end
+  return true
+
 end
 
 # Time Complexity: ?
 # Space Complexity: ?
 def longest_prefix(strings)
-  raise NotImplementedError, "Not implemented yet"
+  return "" if strings.length == 0
+  first = strings.min
+  last  = strings.max
+  index = 0
+  while index < first.length
+    break if first[index] != last[index]
+    index += 1
+  end
+  return first[0...index]
 end
-
