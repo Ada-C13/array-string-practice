c694d6d959a4afeefe2bdf7bdcf69ade29434482
Jessica Stone <jstone4815@gmail.com>
2020-03-09 16:33:36 -0700

    complete longest_prefix and refactor palindrome

--- lib/practice_exercises.rb
+++ lib/practice_exercises.rb
@@ -1,12 +1,11 @@
-# Time Complexity: ?
-# Space Complexity: ?
+# Time Complexity: O(n) - two loops are run, but they are not nested so it is O(2n) which drops the 2 and becomes O(n)
+# Space Complexity: O(1)
 def is_palindrome(string)
   return true if string == ""
 
   new_string = ""
   length = string.length
   valid_characters = /\w/
-
   length.times do |i|
     if string[i].match?(valid_characters)
       new_string += string[i]
@@ -14,26 +13,30 @@ def is_palindrome(string)
   end
 
   length = new_string.length
-
   new_string = new_string.downcase
-
   length.times do |i|
     first = new_string[i]
     last = new_string[length-1]
-    if first == last
-      length -= 1
-      next
-    else
-      return false
-    end
+    return false if first != last
+    length -= 1
   end
 
   return true
 end
 
-# Time Complexity: ?
-# Space Complexity: ?
+# Time Complexity: O(n^2) - there is a nested loop so this is O(n^2)
+# Space Complexity: O(1)
 def longest_prefix(strings)
-  raise NotImplementedError, "Not implemented yet"
-end
+  match  = ""
+  shortest = strings.min_by(&:length).length
 
+  shortest.times do |i|
+    letter = strings[0][i]
+    strings.each do |word|
+      word[i] == letter ? next : (return match)
+    end
+    match += letter
+  end
+
+  return match
+end
