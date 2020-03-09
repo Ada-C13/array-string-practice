14fd791f3fdc02701459a9f66a327bcaa6ffda1a
Jessica Stone <jstone4815@gmail.com>
2020-03-09 16:06:40 -0700

    submit solution for palindrome

--- lib/practice_exercises.rb
+++ lib/practice_exercises.rb
@@ -1,8 +1,34 @@
-
 # Time Complexity: ?
 # Space Complexity: ?
 def is_palindrome(string)
-  raise NotImplementedError, "Not implemented yet"
+  return true if string == ""
+
+  new_string = ""
+  length = string.length
+  valid_characters = /\w/
+
+  length.times do |i|
+    if string[i].match?(valid_characters)
+      new_string += string[i]
+    end
+  end
+
+  length = new_string.length
+
+  new_string = new_string.downcase
+
+  length.times do |i|
+    first = new_string[i]
+    last = new_string[length-1]
+    if first == last
+      length -= 1
+      next
+    else
+      return false
+    end
+  end
+
+  return true
 end
 
 # Time Complexity: ?
