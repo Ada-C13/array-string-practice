096c22487c1145119ea9cab672957216a973f15c
Maryam Heshmati <mairheshmati@gmail.com>
2020-03-10 09:12:54 -0700

    palindrome and longest_common_prefix methods finilized- all tests running

--- lib/practice_exercises.rb
+++ lib/practice_exercises.rb
@@ -1,13 +1,45 @@
-
-# Time Complexity: ?
-# Space Complexity: ?
+# Time Complexity: O(n)
+# Space Complexity: O(n)
 def is_palindrome(string)
-  raise NotImplementedError, "Not implemented yet"
+  string_array = string.split("")
+  my_char_array = string_array.select do |char|
+    /[a-zA-Z]/.match char
+  end
+
+  first_index = 0
+  last_index = my_char_array.length - 1
+  while first_index < last_index
+    if my_char_array[first_index].upcase == my_char_array[last_index].upcase
+      first_index += 1
+      last_index -= 1
+    else
+      return false
+    end
+  end
+  return true
 end
 
-# Time Complexity: ?
-# Space Complexity: ?
+# Time Complexity: O(n^2)
+# Space Complexity: O(n)
+
 def longest_prefix(strings)
-  raise NotImplementedError, "Not implemented yet"
+  longest_common_prefix = ""
+
+  if strings == nil || strings.length == 0
+    return longest_common_prefix
+  end
+
+  strings[0].each_char.with_index do |char, i|
+    strings.each do |word|
+      if char != word[i] #second word in array
+        return longest_common_prefix
+      end
+    end
+    longest_common_prefix << char
+    i += 1
+  end
+
+  return longest_common_prefix
 end
+
 
