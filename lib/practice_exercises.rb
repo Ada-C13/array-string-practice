52ab8a8f6b8d47b7f6c25bf52249f2040c902450
quinqu <quinruby0@gmail.com>
2020-03-10 07:24:11 -0700

    implemented methods

--- lib/practice_exercises.rb
+++ lib/practice_exercises.rb
@@ -2,12 +2,49 @@
 # Time Complexity: ?
 # Space Complexity: ?
 def is_palindrome(string)
-  raise NotImplementedError, "Not implemented yet"
+  string_array = string.split("") 
+  alpha_only = []
+  alpha_copy = []
+  pattern = /^[A-Za-z]+$/
+  string_array.each do |char|
+    if char.match?(pattern)
+      alpha_only << char.downcase 
+      alpha_copy << char.downcase
+    end 
+  end 
+  length = alpha_only.length
+  (length/2).times do |index|
+    temp = alpha_only[index]
+    alpha_only[index] = alpha_only[length - 1 - index]
+    alpha_only[length - 1 - index] = temp
+  end 
+  return alpha_only.join() == alpha_copy.join()
 end
 
 # Time Complexity: ?
 # Space Complexity: ?
 def longest_prefix(strings)
-  raise NotImplementedError, "Not implemented yet"
+  index = 0 
+  prefix = ""
+  all_true = false 
+  st = strings.min_by(&:length) 
+  st.split("").each do |char| 
+    strings.each do |string| #string.split.each do char???
+      if string.split("")[index] == char
+        all_true = true 
+      else
+        all_true = false
+      end
+    end
+
+    if all_true == false 
+      return prefix
+    end 
+    if all_true == true
+      prefix += char
+    end
+    index += 1
+  end 
+  return prefix
 end
 
