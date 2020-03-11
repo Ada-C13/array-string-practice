52bcd148561395eada707ef2705c2eff34b803e9
Katie <katie.vandervoort@gmail.com>
2020-03-11 15:34:18 -0700

    wrote code for is_plaindrom and longest_prefix

--- lib/practice_exercises.rb
+++ lib/practice_exercises.rb
@@ -1,13 +1,40 @@
 
-# Time Complexity: ?
-# Space Complexity: ?
+# Time Complexity: O(n)
+# Space Complexity: O(1)
 def is_palindrome(string)
-  raise NotImplementedError, "Not implemented yet"
+  alph_str = string.downcase.gsub(/[^a-z\w]/,'') #substitute any non-letter or whitespace with nothing
+  alph_str.length/2.times do |index|
+    if alph_str[index] != alph_str[-1 - index]
+      return false
+    end
+    return true
+  end
+
 end
 
-# Time Complexity: ?
-# Space Complexity: ?
+# Time Complexity: O(n^2)
+# Space Complexity: O(1)
 def longest_prefix(strings)
-  raise NotImplementedError, "Not implemented yet"
-end
+  return strings [0] if strings.length == 1
 
+  common_prefix = ""
+  index = 0
+
+  until strings[0][index] != strings[1][index] || (strings[0][index] == nil && strings[1][index] == nil)
+    common_prefix += strings[0][index]
+    index += 1
+  end
+
+  strings.each do |string|
+    return "" if common_prefix[0] != string[0]
+
+    common_prefix.length.times do |index|
+      if common_prefix[index] != string[index]
+        common_prefix = common_prefix[0...index]
+        break
+      end
+    end 
+  end
+
+  return common_prefix
+end
