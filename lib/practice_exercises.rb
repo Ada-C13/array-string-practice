5889df16fa707804d18f74e1034f89a96edd264d
Angela Nguyen <angethuy@gmail.com>
2020-03-10 20:16:53 -0700

    finished exercises

--- lib/practice_exercises.rb
+++ lib/practice_exercises.rb
@@ -1,13 +1,29 @@
 
-# Time Complexity: ?
-# Space Complexity: ?
+# Time Complexity: O(n)
+# Space Complexity: O(n), regex results return a new array
 def is_palindrome(string)
-  raise NotImplementedError, "Not implemented yet"
+  processed_string = string.scan(/\w/)
+
+  return false if !(processed_string.length >= 0)
+
+  (processed_string.length/2).times do |x|
+    return false if processed_string[0+x].downcase != processed_string[-(1+x)].downcase
+  end
+
+  return true
 end
 
-# Time Complexity: ?
-# Space Complexity: ?
+# Time Complexity: O(n^2) because nested times loops
+# Space Complexity: O(1), no additional data structures used
 def longest_prefix(strings)
-  raise NotImplementedError, "Not implemented yet"
+  leader = strings[-1]
+
+  leader.length.times do |i|
+    strings.length-1.times do |j|
+      return leader[0,i] if leader[i] != strings[j][i]
+    end
+  end
+  
+  return leader
 end
 
