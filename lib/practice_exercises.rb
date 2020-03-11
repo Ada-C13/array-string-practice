49cdcb7f1f0f864f0a1929c6fe39c246b5e60400
saraleecodes <sara.lee.higgins@gmail.com>
2020-03-11 16:18:27 -0700

    adds a reverse-in-place function to #is_palindrome

--- lib/practice_exercises.rb
+++ lib/practice_exercises.rb
@@ -2,7 +2,19 @@
 # Time Complexity: ?
 # Space Complexity: ?
 def is_palindrome(string)
-  raise NotImplementedError, "Not implemented yet"
+  first_index = 0
+  last_index = string.length - 1
+  palindrome_word = string
+
+	while first_index < last_index
+		temp = string[first_index]
+		string[first_index] = string[last_index]
+		string[last_index] = temp
+		first_index += 1
+		last_index -= 1
+  end
+  
+  return palindrome_word == string
 end
 
 # Time Complexity: ?
