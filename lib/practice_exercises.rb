be99f7fa69db6e86a1f782b08421b9d6c4ea3ce9
Faezeh Ashtiani <faa@Faezehs-MacBook-Pro.local>
2020-03-10 21:11:04 -0700

    adds both methods

--- lib/practice_exercises.rb
+++ lib/practice_exercises.rb
@@ -1,13 +1,58 @@
 
-# Time Complexity: ?
-# Space Complexity: ?
-def is_palindrome(string)
-  raise NotImplementedError, "Not implemented yet"
+def reverser(string)
+  first_index = 0
+  last_index = string.length - 1
+  while first_index < last_index
+    string[first_index], string[last_index] = string[last_index], string[first_index]
+    first_index += 1
+    last_index -= 1
+  end
+  return string
 end
 
-# Time Complexity: ?
-# Space Complexity: ?
-def longest_prefix(strings)
-  raise NotImplementedError, "Not implemented yet"
+# Time Complexity: O(n) because we go through the length of the array for comparison.
+# Space Complexity: O(n) because we create a copy of the string to compare it to.
+def is_palindrome(string)
+  joined_str = string.gsub(/[^0-9a-z]/i, '').downcase
+  reverse_str = reverser(joined_str.clone)
+  difference_count = 0
+  joined_str.length.times do |i|
+    difference_count += 1 if joined_str[i] != reverse_str[i]
+  end
+
+  if difference_count != 0
+    return false
+  else
+    return true
+  end
+
 end
+
+
+# Time Complexity: O(n) we will be itterating through the array one or more times.
+# Space Complexity: O(1) because we are not making a new array.
+def longest_prefix(strings)
+  template = strings[0]
+  i = 0
+  
+  until template[i].nil? do
+    comparison_result = true
+    strings[1..].each do |word|
+       
+      if word[i] != template[i]
+        comparison_result = false
+        break
+      end 
+
+    end
+
+    if comparison_result
+      i += 1
+    else 
+      break
+    end
+  end
+  return template[0, i]
+end
+
 
