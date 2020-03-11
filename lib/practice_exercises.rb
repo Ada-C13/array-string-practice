a3d225fa4892da2bb298700bceeea550096c58ad
Jetmire Bajrami <jetmirebajrami@Jetmires-MacBook-Pro.local>
2020-03-10 17:49:22 -0700

    add two methods

--- lib/practice_exercises.rb
+++ lib/practice_exercises.rb
@@ -1,13 +1,40 @@
-
-# Time Complexity: ?
-# Space Complexity: ?
+# Time Complexity: 0(n)?
+# Space Complexity: 0(n)?
 def is_palindrome(string)
-  raise NotImplementedError, "Not implemented yet"
+  string_split = string.upcase.split('')
+  array_letters = string_split.select { |char| /[A-Z]/.match char }
+
+  reverse = []
+  i = array_letters.length
+  while i != 0 do
+    reverse << array_letters[i - 1]
+    i -= 1
+  end
+
+  if reverse == array_letters
+    return true
+  elsif reverse != array_letters
+    return false
+  end
 end
 
-# Time Complexity: ?
-# Space Complexity: ?
+# # Time Complexity: n^2 ?
+# # Space Complexity: O(1) ?
 def longest_prefix(strings)
-  raise NotImplementedError, "Not implemented yet"
+  first_string = strings[0]
+  prefix = ""
+  first_string.each_char.with_index do |char, i|
+    match = 1
+    strings.each do |string|
+      if char != string[i]
+        match = 0  
+      end
+    end
+    if match == 1
+      prefix << char
+    elsif match == 0
+      break
+    end
+  end
+  return prefix
 end
-
