5cecb8d7edf5e921010cba3c106f186c51551ad5
Corinna Fabre <corinna.fabre@gmail.com>
2020-03-11 15:42:07 -0700

    Complete palindromes and prefix exercise.

--- lib/practice_exercises.rb
+++ lib/practice_exercises.rb
@@ -1,13 +1,42 @@
 
-# Time Complexity: ?
-# Space Complexity: ?
+# Time Complexity: O(n)
+# Space Complexity: O(1) - I don't feel confident about this though because of the creation of the comparison array, which I think would double the memory needed.
 def is_palindrome(string)
-  raise NotImplementedError, "Not implemented yet"
+  string = string.downcase.gsub(/[^A-Za-z]/, "").split("")
+  comparison = []
+  i = 0
+  j = -1
+
+  until string[i] == string[j] do
+    comparison << (string[i] == string[j])
+    i += 1
+    j -= 1
+  end
+
+  return comparison.include?(false) == false
 end
 
-# Time Complexity: ?
-# Space Complexity: ?
+# Time Complexity: O(n)
+# Space Complexity: O(1)
 def longest_prefix(strings)
-  raise NotImplementedError, "Not implemented yet"
+  result = ""
+
+  i = 0
+
+  character = strings[0][i]
+
+  while character != nil
+    strings.each do |string|
+      if string[i] != character
+        return result
+      end
+    end
+
+    result += character
+    i += 1
+    character = strings[0][i]
+  end
+
+  return result
 end
 
