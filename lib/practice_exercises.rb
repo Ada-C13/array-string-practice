ff9a528e5b8cd0566814885a95b2a871b6307f0e
Yieni <yaynee8d@gmail.com>
2020-03-11 01:15:00 -0700

    write palindrome method

--- lib/practice_exercises.rb
+++ lib/practice_exercises.rb
@@ -1,13 +1,26 @@
 
-# Time Complexity: ?
-# Space Complexity: ?
+# Time Complexity: O(n)?
+# Space Complexity: O(n) ?
 def is_palindrome(string)
-  raise NotImplementedError, "Not implemented yet"
+  split_string_array = string.downcase.split("")
+  words_array = split_string_array.select{|letter| letter[/[a-zA-z]/]}
+
+  reverse_words_array = []
+  x = 0
+  length = words_array.length
+  until words_array[x] == nil
+    reverse_words_array << words_array[length -(x+1)]
+    x += 1
+  end
+
+  return reverse_words_array == words_array ? true : false
+
 end
 
 # Time Complexity: ?
 # Space Complexity: ?
 def longest_prefix(strings)
-  raise NotImplementedError, "Not implemented yet"
+  array_length = strtings.length
+  
 end
 
