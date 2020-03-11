15769d9a08e2542d8c2cfc50cf7e721e7b9c1849
Halahaddad1 <h.m.haddad@gmail.com>
2020-03-10 22:12:10 -0700

    all tests passing

--- lib/practice_exercises.rb
+++ lib/practice_exercises.rb
@@ -1,13 +1,47 @@
 
-# Time Complexity: ?
-# Space Complexity: ?
+# Time Complexity: O(n)
+# Space Complexity: O(1)
 def is_palindrome(string)
-  raise NotImplementedError, "Not implemented yet"
+  list = []
+
+  c_string = string.gsub(/[^a-zA-Z]/, "").downcase
+  index = c_string.length - 1
+
+  c_string.each_char do |letter|
+    if letter == c_string[index]
+      index -= 1
+    else 
+      list<< 1
+      index -= 1
+    end
+    if index == (c_string.length)/2
+      if 
+        list == [] 
+        return true
+      else 
+        return false
+      end
+    end
+  end
+  return true
 end
 
-# Time Complexity: ?
-# Space Complexity: ?
+# Time Complexity: O(n)
+# Space Complexity: O(1)
 def longest_prefix(strings)
-  raise NotImplementedError, "Not implemented yet"
+  return '' if strings.empty? 
+  index = 0
+  min = strings.min_by{|a| a.size}
+  longest_prefix = ''
+    while index != min.length
+      strings.each do |string|
+          if min[index] != string[index]
+            return longest_prefix
+          end
+        end
+        longest_prefix += min[index]
+        index +=1
+    end
+  return longest_prefix
 end
 
