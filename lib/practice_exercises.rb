45bfac0be6803ca85c1bcb5e8adb07a3d4ebfcef
Sharon Cheung <keikei1128@g.ucla.edu>
2020-03-12 00:23:40 -0700

    complete assignment

--- lib/practice_exercises.rb
+++ lib/practice_exercises.rb
@@ -1,13 +1,33 @@
 
-# Time Complexity: ?
-# Space Complexity: ?
+# Time Complexity: O(n)
+# Space Complexity: O(n)
 def is_palindrome(string)
-  raise NotImplementedError, "Not implemented yet"
+  word_array = string.gsub(/[[:punct:]]/, "").delete(' ')
+  reversed_arrary = []
+  last_index = word_array.length
+  until 0 > last_index
+      reversed_arrary << word_array[last_index]
+      last_index -= 1
+    end
+
+  return word_array.downcase == reversed_arrary.join.downcase 
 end
 
 # Time Complexity: ?
 # Space Complexity: ?
-def longest_prefix(strings)
-  raise NotImplementedError, "Not implemented yet"
-end
 
+strings = ['flow', 'flower', 'flake']
+
+def longest_prefix(strings)
+  prefix = ""
+  strings[0].chars.each_with_index do |letter, index| # looping through flow
+    strings.each_with_index do |word, i| # looping through flow, flower, flake
+      next if i == 0
+      if word[index] != letter
+        return prefix  
+      end
+    end
+    prefix += letter
+  end 
+  return prefix
+end
