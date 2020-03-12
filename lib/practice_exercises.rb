9592d060c2cef7c981e9de555e1917d2e6ac54a9
saraleecodes <sara.lee.higgins@gmail.com>
2020-03-11 17:14:13 -0700

    passes all tests for #is_palindrome

--- lib/practice_exercises.rb
+++ lib/practice_exercises.rb
@@ -2,19 +2,20 @@
 # Time Complexity: ?
 # Space Complexity: ?
 def is_palindrome(string)
+  string.gsub!(/[^a-zA-Z0-9]/, "")
   first_index = 0
   last_index = string.length - 1
-  palindrome_word = string
 
 	while first_index < last_index
-		temp = string[first_index]
+    return false if string[first_index].downcase != string[last_index].downcase
+    temp = string[first_index]
 		string[first_index] = string[last_index]
 		string[last_index] = temp
 		first_index += 1
-		last_index -= 1
+    last_index -= 1
   end
   
-  return palindrome_word == string
+  return true 
 end
 
 # Time Complexity: ?
