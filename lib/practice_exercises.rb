f827b9e2f98292c80b7d35e91a8f0be28c26ff86
Hannah <hannahjoo24@gmail.com>
2020-03-11 21:06:41 -0700

    Modified is_palindrome

--- lib/practice_exercises.rb
+++ lib/practice_exercises.rb
@@ -1,20 +1,18 @@
 
 # Time Complexity: O(n) => Because it depends on the input.
-# From my understanding, the split method also takes O(n). In conclusion, since it drops constant 2, it is O(n)
 
-# Space Complexity: O(n) => Because I created a new array to assign the split string
+# Space Complexity: O(1)
 def is_palindrome(string)
 
   return true if string.empty?
 
   string = string.downcase().gsub(/[^a-z]/, "") 
-  array = string.split("")
 
   i = 0 
-  j = array.length - 1
+  j = string.length - 1
 
   while i < j
-    if array[i] != array[j]
+    if string[i] != string[j]
       return false
     end 
     i += 1 
